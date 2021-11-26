59f9def3b79044f0289a61fd00f316109c4954ab
SMJen1 <83735884+SMJen1@users.noreply.github.com>
2021-11-26 11:21:26 -0800

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 public class simple {
   public static void main(String[] args) {
-	  System.out.println("Hello Team Yactraq with version!");
+	  System.out.println("Hello Team Today Yactraq with version!");
   }
 }
